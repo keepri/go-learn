83f9916c86ff8a6ef53628b2136138a4a8d69dca
KIPRI <hi@kipri.dev>
2023-09-07 23:43:12 +0300

    removed server

--- main.go
+++ main.go
@@ -3,75 +3,70 @@ package main
 import (
 	"fmt"
 
-	// "internal/arrays"
-	// "internal/channels"
-	// "internal/closures"
-	// "internal/concurrency"
-	// condsfuncs "internal/conds-funcs"
-	// "internal/datetest"
-	// "internal/errors"
-	// "internal/generics"
-	// helloworld "internal/hello-world"
-	// "internal/loops"
-	// "internal/maps"
-	// "internal/mutex"
-	// "internal/pointers"
-	// "internal/strings"
-	// "internal/structs"
-	// "internal/variables"
-
-	"github.com/keepri/learn-go/server"
+	"internal/arrays"
+	"internal/channels"
+	"internal/closures"
+	"internal/concurrency"
+	condsfuncs "internal/conds-funcs"
+	"internal/datetest"
+	"internal/errors"
+	"internal/generics"
+	helloworld "internal/hello-world"
+	"internal/loops"
+	"internal/maps"
+	"internal/mutex"
+	"internal/pointers"
+	"internal/strings"
+	"internal/structs"
+	"internal/variables"
 )
 
 func main() {
-	// fmt.Println("\n===================== hello-world =====================")
-	// helloworld.Run()
-	//
-	// fmt.Println("\n===================== strings =====================")
-	// strings.Run()
-	//
-	// fmt.Println("\n===================== datetest =====================")
-	// datetest.Run()
-	//
-	// fmt.Println("\n===================== variables =====================")
-	// variables.Run()
-	//
-	// fmt.Println("\n===================== conds-funcs =====================")
-	// condsfuncs.Run()
-	//
-	// fmt.Println("\n===================== closures =====================")
-	// closures.Run()
-	//
-	// fmt.Println("\n===================== errors =====================")
-	// errors.Run()
-	//
-	// fmt.Println("\n===================== loops =====================")
-	// loops.Run()
-	//
-	// fmt.Println("\n===================== arrays =====================")
-	// arrays.Run()
-	//
-	// fmt.Println("\n===================== structs =====================")
-	// structs.Run()
-	//
-	// fmt.Println("\n===================== maps =====================")
-	// maps.Run()
-	//
-	// fmt.Println("\n===================== pointers =====================")
-	// pointers.Run()
-	//
-	// fmt.Println("\n===================== concurrency =====================")
-	// concurrency.Run()
-	//
-	// fmt.Println("\n===================== channels =====================")
-	// channels.Run()
-	//
-	// fmt.Println("\n===================== mutex =====================")
-	// mutex.Run()
-	//
-	// fmt.Println("\n===================== generics =====================")
-	// generics.Run()
+	fmt.Println("\n===================== hello-world =====================")
+	helloworld.Run()
 
-	fmt.Println("\n===================== server =====================")
-	server.Run()
+	fmt.Println("\n===================== strings =====================")
+	strings.Run()
+
+	fmt.Println("\n===================== datetest =====================")
+	datetest.Run()
+
+	fmt.Println("\n===================== variables =====================")
+	variables.Run()
+
+	fmt.Println("\n===================== conds-funcs =====================")
+	condsfuncs.Run()
+
+	fmt.Println("\n===================== closures =====================")
+	closures.Run()
+
+	fmt.Println("\n===================== errors =====================")
+	errors.Run()
+
+	fmt.Println("\n===================== loops =====================")
+	loops.Run()
+
+	fmt.Println("\n===================== arrays =====================")
+	arrays.Run()
+
+	fmt.Println("\n===================== structs =====================")
+	structs.Run()
+
+	fmt.Println("\n===================== maps =====================")
+	maps.Run()
+
+	fmt.Println("\n===================== pointers =====================")
+	pointers.Run()
+
+	fmt.Println("\n===================== concurrency =====================")
+	concurrency.Run()
+
+	fmt.Println("\n===================== channels =====================")
+	channels.Run()
+
+	fmt.Println("\n===================== mutex =====================")
+	mutex.Run()
+
+	fmt.Println("\n===================== generics =====================")
+	generics.Run()
 }
